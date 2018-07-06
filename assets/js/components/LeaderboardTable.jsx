e50a877ee087b9f6160ed73c8101048f30f672dd
Simon Smith <smithsps@gmail.com>
2018-07-06 17:42:37 -0400

    Improved leaderboard styling.

--- assets/js/components/LeaderboardTable.jsx
+++ assets/js/components/LeaderboardTable.jsx
@@ -5,26 +5,31 @@ class LeaderboardTable extends Component {
   
   state = {
     order: {
-      field: 0,
+      field: "correct",
       ordering: -1
     }
   }
 
   render() {
+    var users = this.props.users.slice()
+    users = users.sort(ordering_sort(this.state.order.field, this.state.order.ordering))
+
     return (
       <div className="leaderboard-table">
-        <table className="table">
+        <table className="table is-fullwidth">
           <thead>
-            <th>Pos</th>
-            <th>Name</th>
-            <th>Correct</th>
-            <th>Total</th>
-            <th>Percentage</th>
+            <tr>
+              <th>Pos</th>
+              <th>Name</th>
+              <th>Correct</th>
+              <th>Total</th>
+              <th>Percentage</th>
+            </tr>
           </thead>
           <tbody>
-            {this.props.users.map((user, i) => (
+            {users.map((user, i) => (
                 <tr key={user.id}>
-                    <th>{i}</th>
+                    <th>{i + 1}</th>
                     <td>{user.name}</td>
                     <td>{user.stats[0].correct}</td>
                     <td>{user.stats[0].total}</td>
@@ -40,6 +45,32 @@ class LeaderboardTable extends Component {
 
 function ratioToPercentage(a, b) {
   return Math.floor(a / b * 1000) / 10
+}
+
+function ordering_sort(field, ordering) {
+  if ("percentage") {
+    return (a, b) => {
+      const a_percentage = ratioToPercentage(a.stats.correct, a.stats.total);
+      const b_percentage = ratioToPercentage(b.stats.correct, b.stats.total);
+      if (a_percentage > b_percentage) {
+        return ordering;
+      }
+      if (a_percentage < b_percentage) {
+        return -1 * ordering;
+      }
+      return 0
+    }
+  } else {
+    return (a, b) => {
+      if (a.stats[0][field] > b.stats[0][field]) {
+        return ordering;
+      }
+      if (a.stats[0][field] < b.stats[0][field]) {
+        return -1 * ordering;
+      }
+      return 0
+    }
+  }
 }
 
 
