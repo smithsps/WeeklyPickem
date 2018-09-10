04e2ac94e23718d6e03a1f49e946f5aa5a8d924a
Simon Smith <smithsps@gmail.com>
2018-09-10 01:20:59 -0400

    Moved from Elm to Bucklescript
    Probably bucklescript-tea

---
+++ apps/web_client/webpack.config.js
@@ -0,0 +1,12 @@
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+
+module.exports = {
+    mode: "development",
+    devtool: 'source-map',
+    entry: './src/main.bs.js',
+    plugins: [
+        new CopyWebpackPlugin([
+            {from: 'static', to: '', force: true},
+        ]),
+    ],
+};
